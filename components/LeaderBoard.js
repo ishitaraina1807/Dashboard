9e6629c6721fafdf0489c710a986c5f0e48c9c74
Ishita <ishitaraina18@gmail.com>
2024-02-18 21:35:41 +0530

    Scroll functionality

--- components/LeaderBoard.js
+++ components/LeaderBoard.js
@@ -1,38 +1,53 @@
-import React from 'react'
+import React from 'react';
 
 const LeaderBoard = () => {
-    const users = [
-        {
-            position: 0o1,
-            name: 'John Doe',
-            xp: "0",
-        },
-        {
-            position: 0o2,
-            name: 'Jane Doe',
-            xp: "0",
-        },
-        {
-            position: 0o3,
-            name: 'John Smith',
-            xp: "0",
-        },
-        {
-            position: 54,
-            name: 'You',
-            xp: "0",
-        },
-    ]
+  const users = [
+    {
+      position: 1,
+      name: 'John Doe',
+      xp: "0",
+    },
+    {
+      position: 2,
+      name: 'Jane Doe',
+      xp: "0",
+    },
+    {
+      position: 3,
+      name: 'John Smith',
+      xp: "0",
+    },
+    {
+      position: 5,
+      name: 'Another User',
+      xp: "0",
+    },
+    {
+      position: 6,
+      name: 'Yet Another User',
+      xp: "0",
+    },
+    {
+      position: 54,
+      name: 'You',
+      xp: "0",
+    },
+  ];
+
   return (
-    <div className='bg-white shadow-lg bg-opacity-10 rounded-xl mt-4 pb-3'>
-      <div className='bg-white shadow-white shadow-sm bg-opacity-20 rounded-xl p-3 text-center text-lg'>
-       Leaderboard
-      </div>
-      <div>
+    <div className='leaderboard-container mt-4 bg-white shadow-lg bg-opacity-10 rounded-xl mt-4 pb-3'>
+              
+      <div className='header bg-white shadow-gray-400 shadow-sm bg-opacity-10 rounded-xl p-3 text-center text-md'>
+        Leaderboard
+      </div> 
+
+      <div className='leaderboard-container' style={{ height: '300px', overflowY: 'auto' }}> 
+      <div className='entries-container' style={{ maxHeight: '300px', overflowY: 'auto' }}>
         {users.map((user, index) => (
           <div
             key={index}
-            className='flex justify-between items-center p-3 mx-3 bg-white shadow-white shadow-sm bg-opacity-20 rounded-xl mt-4 text-center text-sm'
+            className='entry flex justify-between items-center p-3 mx-3 bg-white shadow-gray-400 shadow-sm bg-opacity-10 rounded-xl mt-4 text-center text-sm'
+            style={{ width: 'calc(100% - 20px)' }} 
           >
             <div className='flex items-center gap-2'>
               <div className='flex items-center gap-4'>
@@ -42,14 +57,15 @@ const LeaderBoard = () => {
               </div>
             </div>
             <div className="flex gap-2">
-            <h1>{user.xp}/233</h1>
-            <h1 className='text-yellow'>XP</h1>
+              <h1>{user.xp}/233</h1>
+              <h1 className='text-yellow'>XP</h1>
             </div>
           </div>
         ))}
       </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LeaderBoard
+export default LeaderBoard;
