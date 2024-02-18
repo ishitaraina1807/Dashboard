7a656a75574743ba4c6ec0abc88c0c52907da124
Ishita <ishitaraina18@gmail.com>
2024-02-18 21:25:08 +0530

    Hover Effects

--- components/Box.js
+++ components/Box.js
@@ -5,7 +5,7 @@ import increase from '../public/assets/increase.png'
 
 const Box = ({Date, Title, Description}) => {
   return (
-    <div className='bg-white xl:w-80 w-60 bg-opacity-10 py-3 px-8 shadow-xl border-gray-500 border-[0.1px] rounded-xl text-gray-400'>
+    <div className='bg-white xl:w-80 w-60 bg-opacity-10 py-3 px-8 shadow-xl border-gray-500 border-[0.1px] rounded-xl text-gray-400 hover:bg-opacity-20 hover:cursor-pointer hover:scale-110 duration-300'>
       <p className='text-sm'>Last Practice Linear etc etc</p>
       <p>{Date}</p>
       <Image className='w-28' src={increase} alt='increase' />
